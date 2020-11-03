df818d76e4097a2e1c4cdf40e57bbb9da20d1da3
Tavjpo18 <nguyentien140218@gmail.com>
2020-11-03 19:53:06 +0700

    abc

---
+++ product/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    aws_table_name: 'sanpham',
+    aws_local_config: {
+        region: 'local',
+        endpoint: 'http://18.141.202.243/:3000'
+   
+    },
+    aws_remote_config: {
+      accessKeyId: 'AKIAWAMW4NZRZ3ZFNU7N',
+      secretAccessKey: 'byd/gF3k92xZ7vkN4bHuPPqgjQbUDBNakWQlPZBHtiZK',
+      region: 'ap-southeast-1',
+    }
+}
